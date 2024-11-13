fb658cbe99b519752ad352e82939fbaa790a70d9
Mykhailo Reveko <580735+mikere@users.noreply.github.com>
2024-11-13 14:57:13 +0100

    Display list of users using mat-table

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -2,21 +2,29 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
 
 import { User } from 'src/app/services/model';
 import { AppState, selectUsers } from 'src/app/store';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatTableModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
   users$: Observable<User[]>;
 
-  constructor(private store: Store<AppState>) {
+  displayedColumns = ['name', 'email', 'role', 'status'];
+
+  constructor(private store: Store<AppState>, private router: Router) {
     this.users$ = this.store.select(selectUsers);
+  }
+
+  viewUserDetails(row: User) {
+    this.router.navigate(['/users', row.id]);
   }
 }
