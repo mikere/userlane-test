de4574facf37a55440d416979531b4aab0055ed8
Mykhailo Reveko <580735+mikere@users.noreply.github.com>
2024-11-13 18:11:58 +0100

    Add user list filtering

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,30 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { User } from 'src/app/services/model';
 import { AppState, selectUsers } from 'src/app/store';
 import { Router, RouterModule } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RenderRolePipe } from 'src/app/pipes/render-role.pipe';
+import { RenderStatusPipe } from 'src/app/pipes/render-status.pipe';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatTableModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RenderRolePipe,
+    RenderStatusPipe,
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
-  users$: Observable<User[]>;
-
   displayedColumns = ['name', 'email', 'role', 'status'];
+  userListDataSource = new MatTableDataSource<User>();
+
+  filterControl = new FormControl('');
 
   constructor(private store: Store<AppState>, private router: Router) {
-    this.users$ = this.store.select(selectUsers);
+    this.store
+      .select(selectUsers)
+      .pipe(takeUntilDestroyed())
+      .subscribe((users) => {
+        this.userListDataSource.data = users;
+      });
   }
 
   viewUserDetails(row: User) {
     this.router.navigate(['/users', row.id]);
+  }
+
+  applyFilter() {
+    this.userListDataSource.filter = this.filterControl.value || '';
   }
 }
